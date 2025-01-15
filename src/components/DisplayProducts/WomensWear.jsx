30cf842ad5946900cdd511b4c03b6a0a7756f675
Sourabkansal <sourabkansal56@gmail.com>
2025-01-16 01:19:10 +0530

    add functionality

--- src/components/DisplayProducts/WomensWear.jsx
+++ src/components/DisplayProducts/WomensWear.jsx
@@ -4,6 +4,10 @@ import { productContext } from "../contextStore/ProductStore";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import { FaStar } from "react-icons/fa6";
+import { FaRegStar } from "react-icons/fa6";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
 import { wishlistContext } from "../contextStore/WishlistContext";
 const WomensWear = () => {
   const { wishlistt, setwishlistt } = useContext(wishlistContext);
@@ -48,47 +52,85 @@ const WomensWear = () => {
   console.log(newitemm);
   console.log(wishlistt);
 
-  return (
-    <>
-      <div className=" flex justify-center sm:justify-start flex-wrap gap-5  ">
+  let arr = [];
+  let rating = (rat) => {
+    for (let i = 0; i <= 4; i++) {
+      if (i < rat) {
+        arr[i] = 1;
+      } else {
+        arr[i] = 0;
+      }
+    }
+  };
+
+return (
+    <div>
+      <div className="  flex flex-col gap-6  items-center sm:flex-row justify-center my-10  flex-wrap sm:gap-8   ">
         {newitemm.map((item) => {
           return (
             <NavLink key={item.id} to={`/product/${item.id}`}>
-              <div className="border-2 flex flex-col gap-3 border-gray-500 w-[230px] mt-5 mb-5 ml-5 pl-3 pt-3 lb-3 pr-3  hover:bg-gray-100 dark:bg-gray-200 text-black  ">
-                <div className="h-[230px] w-[205px] border-2 border-gray-400 bg-white ">
-                  <img
-                    className="h-[227px] w-auto m-auto"
-                    src={item.image}
-                  ></img>
-                </div>
-                <div className="flex justify-between">
-                  <div className=" whitespace-nowrap overflow-hidden text-ellipsis w-[70%] ">
-                    <h1 className=" font-semibold ">{item.subcategory}</h1>
-                    <p className="text-sm text-gray-700">{item.name}</p>
+              <div>
+                <div className=" bg-white  flex flex-col justify-center items-center p-4 w-60 sm:w-56 border border-gray-300 rounded-lg  shadow-md hover:shadow-xl transition-all duration-200 ">
+                  <div className="text-xs flex justify-end w-56 sm:w-48 font-semibold text-gray-400">
+                    id:{item.id}
                   </div>
-                  <div>
-                    <p className="font-semibold font-sans">{`$ ${item.price}`}</p>
-                  </div>
-                </div>
-                <div className="flex mb-3">
-                  <NavLink to={"/WomensWear"}>
-                    <div
-                      onClick={() => addWishlist(item.id)}
-                      className="h-[30px] w-[30px] bg-black text-white border-2 border-gray-400 pt-1 pl-1"
-                    >
-                      {/* <FaRegHeart className={item.infav ? "bg-red-500" : ""} /> */}
 
-                      {item.infav ? (
-                        <FaHeart className="text-[#FF3131] text-[18px]" />
-                      ) : (
-                        <FaRegHeart className="text-[18px]" />
-                      )}
+                  <div className="relative">
+                    <img
+                      src={item.image}
+                      alt=""
+                      className=" h-44 w-40 sm:w-36"
+                    />
+                    <NavLink to={"/WomensWear"}>
+                      <div
+                        onClick={() => addWishlist(item.id)}
+                        className="p-[7px] bg-gray-800 h-8 w-8 rounded-full text-white absolute top-4 right-[-35px] sm:right-[-30px]"
+                      >
+                        {/* <FaRegHeart className={item.infav ? "bg-red-500" : ""} /> */}
+                        {item.infav ? (
+                          <FaHeart className="text-[#FF3131] text-[18px]" />
+                        ) : (
+                          <FaRegHeart className="text-[18px]" />
+                        )}
+                      </div>
+                    </NavLink>
+                  </div>
+                  <div className="flex flex-col justify-start  w-48 sm:w-44 mt-3">
+                    <div className="">
+                      {" "}
+                      <h1 className="font-bold">{item.subcategory}</h1>
                     </div>
-                  </NavLink>
-                  <div className="w-[88%] ml-2">
-                    <button className="border-2 border-black h-[30px] w-[100%] bg-white font-semibold text-xs">
-                      Add to cart
-                    </button>
+
+                    <div className="flex text-sm justify-start items-center">
+                      {rating(item.rating)}
+                      {arr.map((item) => {
+                        if (item == 1) {
+                          return <FaStar />;
+                        } else {
+                          return <FaRegStar />;
+                        }
+                      })}
+                      
+                      <span className="px-1 font-semibold">{item.rating}</span>
+                    </div>
+                  </div>
+                  {/* div in div */}
+                  <div className="flex justify-between w-48 sm:w-44 mt-6">
+                    <div className="">
+                      <div className=" text-xs ">
+                        <span className="line-through font-semibold ">{`$ ${(
+                          item.price +
+                          item.price * (10 / 100)
+                        ).toFixed(2)}`}</span>
+                        <span className="no-underline text-xs bg-blue-200 px-1  ml-1 rounded-sm">
+                          -10%
+                        </span>
+                      </div>
+                      <div className="font-bold">{`$ ${item.price}`}</div>
+                    </div>
+                    <div className="flex justify-center items-center text-2xl bg-blue-700 w-10 h-10 text-white rounded-md shadow-md hover:shadow-lg transition-shadow duration-200 ">
+                      <MdOutlineShoppingCart />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -96,7 +138,7 @@ const WomensWear = () => {
           );
         })}
       </div>
-    </>
+    </div>
   );
 };
 
